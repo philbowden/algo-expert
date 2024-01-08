645201148c7634644d689b9446e69438f6b3ec82
Phil Bowden <phil.g.bowden@gmail.com>
2024-01-07 17:44:39 -0800

    missing numbers, majority element, sweetAndSavory.

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,82 @@
 import java.util.*;
 
+import static java.util.Collections.sort;
+
 class Program {
     public static void main(String[] args) {
         Program p = new Program();
-        int[] nums = {1, -1};
-        System.out.println(p.zeroSumSubarray(nums));
+        int[] array = {2, 5, -4, -7, 12, 100, -25};
+        System.out.println(Arrays.toString(p.sweetAndSavory(array, -20)));
+    }
+    public int[] sweetAndSavory(int[] dishes, int target) {
+        int[] best = {0,0};
+        if (dishes.length < 2) {
+            return best;
+        }
+        int closest = Integer.MAX_VALUE;
+        Arrays.sort(dishes);
+        int sweet = 0;
+        int savory = dishes.length-1;
+        while(dishes[sweet] < 0 && dishes[savory] > 0) {
+            int currentVal = dishes[sweet] + dishes[savory];
+            if (currentVal <= target) {
+                if (target - currentVal < closest) {
+                    closest = target - currentVal;
+                    best = new int[] {dishes[sweet], dishes[savory]};
+                }
+                sweet++;
+            } else {
+                savory--;
+            }
+        }
+        return best;
+    }
+
+    public int majorityElement(int[] array) {
+        int candidate = -1;
+        int count = 0;
+        for (int num : array) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+
+    public int[] missingNumbers(int[] nums) {
+        int n = nums.length;
+        int totalMissingAmount = 0;
+        for (int i = 1; i <= n + 2; i++) {
+            totalMissingAmount += i;
+        }
+        for (int num : nums) {
+            totalMissingAmount -= num;
+        }
+        int averageMissingValue = totalMissingAmount / 2;
+        int foundLeftHalf = 0;
+        int foundRightHalf = 0;
+        for (int num : nums) {
+            if (num <= averageMissingValue) {
+                foundLeftHalf += num;
+            } else {
+                foundRightHalf += num;
+            }
+        }
+        int expectedLefthalf = 0;
+        int expectedRightHalf = 0;
+        for (int i = 1; i <= n + 2; i++) {
+            if (i <= averageMissingValue) {
+                expectedLefthalf += i;
+            } else {
+                expectedRightHalf += i;
+            }
+        }
+        return new int[]{expectedLefthalf - foundLeftHalf, expectedRightHalf - foundRightHalf};
     }
 
     public boolean zeroSumSubarray(int[] nums) {
